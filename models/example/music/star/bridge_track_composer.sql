9eb6c63635263c5809b806a6ea389e91b0ee8254
Ivica <ivicanigay@gmail.com>
2026-04-29 20:30:49 +0000

    corrections

--- models/example/music/star/bridge_track_composer.sql
+++ models/example/music/star/bridge_track_composer.sql
@@ -5,7 +5,7 @@ select
     dc.composer_id::number   as composer_id
 from {{ source('Music_Data', 'TRACK') }} t,
 lateral flatten(input => split(t.composer, ';')) f
-join {{ ref('create_table_dim_composer') }} dc
+join {{ ref('dim_composer') }} dc
     on trim(f.value::string) = dc.name
 where t.composer is not null
 and trim(f.value::string) <> ''
